ed28dc7ef227954c52ed3493b30b38cd0981210e
Michiel van Liempt <michiel.v.liempt@c.zen.ly>
2023-04-12 12:23:30 +0200

    generate empty project
    I updated the dependency versions

---
+++ app/src/main/java/nl/michiel/portfolio/MainActivity.kt
@@ -0,0 +1,17 @@
+package nl.michiel.portfolio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import nl.michiel.portfolio.ui.theme.PortfolioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PortfolioTheme {
+//                TopLevelNavigation()
+            }
+        }
+    }
+}
