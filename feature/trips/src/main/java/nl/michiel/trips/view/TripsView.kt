57f0577a01a592502ae150abf138d379a30bce9d
Michiel van Liempt <michiel.v.liempt@c.zen.ly>
2023-04-17 11:59:53 +0200

    show map
    key stored in global gradle properties

--- feature/trips/src/main/java/nl/michiel/trips/view/TripsView.kt
+++ feature/trips/src/main/java/nl/michiel/trips/view/TripsView.kt
@@ -1,22 +1,55 @@
 package nl.michiel.trips.view
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MarkerInfoWindow
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import timber.log.Timber
+
+val amsterdam = LatLng(52.369792, 4.89075924)
 
 @Composable
 fun TripsView() {
     Timber.d("maps key: ")
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
     ) {
-        Text("we'll have a map here soon")
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(amsterdam, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+
+            ) {
+            MarkerInfoWindow(
+                state = MarkerState(position = amsterdam),
+                title = "Amsterdam",
+                snippet = "bla bla bla bla",
+            ) { marker ->
+                Column(
+                    Modifier
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.DarkGray)
+                        .padding(8.dp)) {
+                    marker.title?.let { Text(it) }
+                    with(marker.position) { Text("$latitude $longitude") }
+                }
+            }
+        }
     }
 }
